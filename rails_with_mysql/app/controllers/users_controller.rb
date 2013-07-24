eb870895945037fa23e2a334e993b7d118c5caed
PravinKShepHertz <pravin.kumar@shephertz.co.in>
2013-07-24 18:20:04 +0530

    applied css to rails_mysql app

--- rails_with_mysql/app/controllers/users_controller.rb
+++ rails_with_mysql/app/controllers/users_controller.rb
@@ -25,11 +25,11 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
+    
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
+        format.html { redirect_to users_path }
+        format.json { render action: 'index', status: :created, location: @user }
       else
         format.html { render action: 'new' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to users_path }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :phone, :biodata)
+      params.require(:user).permit(:name, :email, :desc)
     end
 end
