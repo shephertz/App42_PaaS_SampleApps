241308a21bd586dc412cd20fb94ffb68f35c61bc
PravinKShepHertz <pravin.kumar@shephertz.co.in>
2013-08-07 12:49:01 +0530

    added rails sample app

---
+++ rails_sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSampleApp::Application.config.secret_token = 'a5adc3120e8e79e01f054ab83d3f97a8a323f307aaad803093829d33c5f10af82fe0bf2d2146ec2a76e52eec0ce1154d85f6ed53d8224f2985e7952a0d34bee1'
