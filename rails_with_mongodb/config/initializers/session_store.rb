1c1cae6c816a5def7a5dbed4af49dee515b65ec3
PravinKShepHertz <pravin.kumar@shephertz.co.in>
2013-08-08 13:11:01 +0530

    added new rails_with_mongodb application

--- rails_with_mongodb/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-RailsWithMongodb::Application.config.session_store :cookie_store, key: '_rails_with_mongodb_session'
---
+++ rails_with_monodb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsWithMonodb::Application.config.secret_token = '022da0a4513357a7789f0007818931da04de116c1be9a8d0085ebd677b38b57241b454b742f8ba4c4e29c49384ab40d216105c2ca5d28a6b9afa760d6b4fe2f0'
