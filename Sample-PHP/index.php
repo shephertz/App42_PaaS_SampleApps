04adab0a5a8746d4b276ae79c2f8591ea53bcdf5
Rohit Raina <rohit.raina@shephertz.co.in>
2013-08-19 19:20:45 +0530

    updated SAMPLE-PHP application

--- Sample-PHP/index.php
+++ Sample-PHP/index.php
@@ -1,3 +1,4 @@
+<?php
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -9,3 +10,4 @@
 <div style="width:50%; margin:200px auto 0 auto; padding:50px; text-align:center; font-family: 'Open Sans'; font-size:40px; color:#00b3ad; border:2px solid #815512; border-radius: 5px 5px 5px 5px;"> Welcome to App42 PaaS </div>
 </body>
 </html>
+?>
