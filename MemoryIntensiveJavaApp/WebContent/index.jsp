c226c5da68d0ea7d92c70e40dca317017cf177f0
Rohit Raina <rohit.raina@shephertz.co.in>
2013-08-28 20:12:17 +0530

    Added MemoryIntensiveJavaApp

---
+++ MemoryIntensiveJavaApp/WebContent/index.jsp
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>App42 Memory-Intensive App</title>
+</head>
+<body>
+	<form method="post" action="save">
+	<h3>Click to start the memory intensive process</h3>
+		<input id="SubmitButton" type="image"
+			name="submit" alt="Submit" src="images/submit_btn.png"></input>
+	</form>
+	
+	<form method="get" action="save">
+	<h3>Click to get the current memory usage</h3>
+		<input id="SubmitButton" type="image"
+			name="getUsage" alt="Submit" src="images/submit_btn.png"></input>
+	</form>
+</body>
+</html>
---
+++ MemoryIntensiveJavaApp/src/com/shephertz/app42/paas/sample/FormSave.java
@@ -0,0 +1,54 @@
+package com.shephertz.app42.paas.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FormSave This class saves the data into database
+ */
+public class FormSave extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FormSave() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String usage = new MemoryTask().Info();
+		System.out.println("Usage: " + usage);
+	    out.print("<!doctype html><html><head><meta charset='utf-8'><title>App42 CPU-Intensive App</title><body><h2>CPU Usage: "+usage+" </h2></body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 * 
+	 *      This functions saves the data into user table
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		while (true) {
+			MemoryTask.load();
+		}
+
+	}
+
+}
